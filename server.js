02d2731a567e6f78afe991a191e4990895708683
yuvrajmakwana77 <153734802+yuvrajmakwana77@users.noreply.github.com>
2024-05-09 16:10:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const server = require("http").createServer(app);
+// const io = require("socket.io")(server);
+// app.use(express.static(path.join(__dirname+"/public")));
+// io.on("connection",function(socket){
+//     socket.on("newuser",function(username){
+//         socket.broadcast.emit("update",username + "Joined the conversation");
+//     });
+//     socket.on("exituser",function(username){
+//         socket.broadcast.emit("update",username + "left the conversation");
+//     });
+//     socket.on("chat",function(message){
+//         socket.broadcast.emit("chat",message);
+//     });
+// });
+// server.listen(5000);
+
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-app.use(express.static(path.join(__dirname+"/public")));
-io.on("connection",function(socket){
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update",username + "Joined the conversation");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("update", `${username} joined the conversation`);
     });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update",username + "left the conversation");
+
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("update", `${username} left the conversation`);
     });
-    socket.on("chat",function(message){
-        socket.broadcast.emit("chat",message);
+
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
     });
 });
-server.listen(5000);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
